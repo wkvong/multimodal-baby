46a42f5b5b021a5de2594cd0058ebed117af840b
Wai Keen Vong <waikeenvong@gmail.com>
2021-06-25 15:29:25 -0400

    adding new test script

---
+++ test.py
@@ -0,0 +1,5 @@
+from multimodal_saycam.data.base_data_module import BaseDataModule, load_and_print_info
+from multimodal_saycam.data.multimodal_data_module import MultiModalDataModule
+
+mm = MultiModalDataModule()
+load_and_print_info(MultiModalDataModule)
