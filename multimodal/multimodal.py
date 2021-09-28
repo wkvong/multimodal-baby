bd0655ebe04727e3f2f13052dbe03e918d103da7
Wai Keen Vong <waikeenvong@gmail.com>
2021-09-28 14:54:07 -0400

    implemented flat embedding model variant

--- multimodal/multimodal.py
+++ multimodal/multimodal.py
@@ -32,6 +32,7 @@ class VisionEncoder(nn.Module):
         super().__init__()
         self.args = vars(args) if args is not None else {}
 
+        self.embedding_type = self.args.get("embedding_type")
         self.embedding_dim = self.args.get("embedding_dim")
         self.pretrained_cnn = self.args.get("pretrained_cnn")
         self.finetune_cnn = self.args.get("finetune_cnn")
@@ -76,9 +77,14 @@ class VisionEncoder(nn.Module):
         else:
             print('Fine-tuning CNN layers!')  # fine-tune cnn
 
-        # remove classifier head and add 1x1 convolution to map original embedding (2048) to embedding_dim
-        model = torch.nn.Sequential(*list(model.children())[:-2],
-                                    nn.Conv2d(2048, self.embedding_dim, 1))
+        if self.embedding_type == "spatial":
+            # remove classifier head and add 1x1 convolution to map original embedding (2048) to embedding_dim
+            model = torch.nn.Sequential(*list(model.children())[:-2],
+                                        nn.Conv2d(2048, self.embedding_dim, 1))
+        elif self.embedding_type == "flat":
+            # remove classifier head and add linear layer to map original embedding (2048) to embedding_dim
+            model.fc = nn.Linear(2048, self.embedding_dim)
+
         return model
 
 
@@ -91,6 +97,7 @@ class TextEncoder(nn.Module):
         self.args = vars(args) if args is not None else {}
 
         self.text_encoder = self.args.get("text_encoder")
+        self.embedding_type = self.args.get("embedding_type")
         self.input_dim = self.args.get("input_dim")
         self.embedding_dim = self.args.get("embedding_dim")
         self.hidden_dim = self.args.get("hidden_dim")
@@ -103,29 +110,62 @@ class TextEncoder(nn.Module):
         
     def forward(self, x, x_len):
         if self.text_encoder == "embedding":
-            output = self.embedding(x)  # (B, L, E)
-            return output
+            if self.embedding_type == "flat":
+                # flat embedding for embedding only model
+                output = self.embedding(x)  # (B, L, E)
+
+                # calculate mean embedding per utterance
+                output = torch.sum(output, dim=1)  # first sum over length dim, (B, E)
+                output = torch.div(output, x_len.unsqueeze(1))  # then divide by utterance length, (B, E)
+                
+                return output
+            elif self.embedding_type == "spatial":
+                # spatial embedding for embedding only model
+                output = self.embedding(x)  # (B, L, E)
+                return output
         elif self.text_encoder == "lstm":
-            # initialize hidden state
-            batch_size = x.size(0)
-            hidden = self.init_hidden(batch_size)
+            if self.embedding_type == "flat":
+                # flat embedding for biLSTM using final hidden states
+                # initialize hidden state
+                batch_size = x.size(0)
+                hidden = self.init_hidden(batch_size)
+     
+                # embed padded sequence and pack
+                embedding = self.embedding(x)  # (B, L, E)
+                embedding = embedding.view(-1, batch_size, self.embedding_dim)  # (L, B, E)
+                # need to move x_len to cpu for this line to work
+                embedding = pack_padded_sequence(embedding, x_len.cpu(), enforce_sorted=False)
+     
+                # pass through lstm
+                output, (hidden, cell) = self.lstm(embedding, hidden)
 
-            # embed padded sequence and pack
-            embedding = self.embedding(x)  # (B, L, E)
-            embedding = embedding.view(-1, batch_size, self.embedding_dim)  # (L, B, E)
-            # need to move x_len to cpu for this line to work
-            embedding = pack_padded_sequence(embedding, x_len.cpu(), enforce_sorted=False)
-
-            # pass through lstm
-            output, _ = self.lstm(embedding, hidden)
-
-            # unpack and reshape
-            output, _  = pad_packed_sequence(output)  # (L, B, 2*E)
-            output_fwd = output[:, :, :self.embedding_dim]  # get hidden states from forward pass
-            output_bwd = output[:, :, self.embedding_dim:]  # get hidden states from backward pass
-            output_avg = torch.mean(torch.stack([output_fwd, output_bwd]), dim=0)  # take average
-            output = output.view(batch_size, -1, self.embedding_dim)  # (B, L, E)
-            return output
+                # get final hidden state by averaging forward and backward passes
+                hidden_fwd = hidden[0, :, :]  # (B, E)
+                hidden_bwd = hidden[1, :, :]  # (B, E)
+                hidden = torch.mean(torch.stack([hidden_fwd, hidden_bwd]), dim=0)  # (B, E)
+                return hidden
+            elif self.embedding_type == "spatial":
+                # spatial embedding for biLSTM using all hidden states
+                # initialize hidden state
+                batch_size = x.size(0)
+                hidden = self.init_hidden(batch_size)
+     
+                # embed padded sequence and pack
+                embedding = self.embedding(x)  # (B, L, E)
+                embedding = embedding.view(-1, batch_size, self.embedding_dim)  # (L, B, E)
+                # need to move x_len to cpu for this line to work
+                embedding = pack_padded_sequence(embedding, x_len.cpu(), enforce_sorted=False)
+     
+                # pass through lstm
+                output, _ = self.lstm(embedding, hidden)
+     
+                # unpack and reshape
+                output, _  = pad_packed_sequence(output)  # (L, B, 2*E)
+                output_fwd = output[:, :, :self.embedding_dim]  # get hidden states from forward pass
+                output_bwd = output[:, :, self.embedding_dim:]  # get hidden states from backward pass
+                output_avg = torch.mean(torch.stack([output_fwd, output_bwd]), dim=0)  # take average
+                output = output.view(batch_size, -1, self.embedding_dim)  # (B, L, E)
+                return output
 
     def _forward_unbatched(self, x, x_len):
         if self.text_encoder == "embedding":
@@ -151,6 +191,7 @@ class MultiModalModel(nn.Module):
 
         self.text_encoder = self.args.get("text_encoder", TEXT_ENCODER)
         self.sim = self.args.get("sim", SIM)
+        self.embedding_type = self.args.get("embedding_type", EMBEDDING_TYPE)
         self.normalize_features = self.args.get("normalize_features", NORMALIZE_FEATURES)
 
         self.image_embed = VisionEncoder(args=args)
@@ -170,26 +211,39 @@ class MultiModalModel(nn.Module):
         return self.text_embed(text, text_length)
         
     def forward(self, image, text, text_length, self_distillation=False, teacher=False):
-        # encode image and text
-        image_features = self.encode_image(image)  # (B, E, H, W,)
-        text_features = self.encode_text(text, text_length)  # (B, L, E)
+        if self.embedding_type == "flat":
+            # encode image and text as flat embeddings
+            image_features = self.encode_image(image)  # (B, E)
+            text_features = self.encode_text(text, text_length)  # (B, E)
 
-        if self.normalize_features:
-            image_features = F.normalize(image_features, p=2, dim=1)  # normalize image features
-            text_features = F.normalize(text_features, p=2, dim=2)  # normalize text features
+            if self.normalize_features:
+                image_features = F.normalize(image_features, p=2, dim=1)  # normalize image features
+                text_features = F.normalize(text_features, p=2, dim=1)  # normalize text features
 
-        # calculate batched similarity
-        if self.sim == "mean":
-            # mean similarity takes the dot product (sum) of all spatial image and word
-            # embeddings, and then takes the average across all words and spatial locations
-            match_sum = torch.einsum('iehw,tle->it', [image_features, text_features])  # calculate matchmap
-            match_avg = match_sum / (7 * 7 * text_length)  # divide by h, w, l
-        elif self.sim == "max":
-            # max similarity takes the maximum dot product for all spatial embeddings
-            # for a given word, and then averages across words
-            match_max = torch.einsum('iehw,tle->itlhw', [image_features, text_features])  # calculate matchmap
-            match_max = torch.amax(match_max, dim=(3, 4))  # amax to apply over multiple dims
-            match_avg = torch.sum(match_max, dim=2) / text_length  # divide by h, w, l
+            # calculate match similarity
+            match = image_features @ text_features.T
+            print(match.size())
+        elif self.embedding_type == "spatial":
+            # encode image and text as spatial embeddings
+            image_features = self.encode_image(image)  # (B, E, H, W,)
+            text_features = self.encode_text(text, text_length)  # (B, L, E)
+     
+            if self.normalize_features:
+                image_features = F.normalize(image_features, p=2, dim=1)  # normalize image features
+                text_features = F.normalize(text_features, p=2, dim=2)  # normalize text features
+     
+            # calculate batched similarity
+            if self.sim == "mean":
+                # mean similarity takes the dot product (sum) of all spatial image and word
+                # embeddings, and then takes the average across all words and spatial locations
+                match_sum = torch.einsum('iehw,tle->it', [image_features, text_features])  # calculate matchmap
+                match = match_sum / (7 * 7 * text_length)  # divide by h, w, l
+            elif self.sim == "max":
+                # max similarity takes the maximum dot product for all spatial embeddings
+                # for a given word, and then averages across words
+                match_max = torch.einsum('iehw,tle->itlhw', [image_features, text_features])  # calculate matchmap
+                match_max = torch.amax(match_max, dim=(3, 4))  # amax to apply over multiple dims
+                match = torch.sum(match_max, dim=2) / text_length  # divide by h, w, l
 
         # transform to logits and scale with temperature param (either infonce or kl temp)
         if self_distillation:
@@ -200,8 +254,8 @@ class MultiModalModel(nn.Module):
         else:
             logit_scale = self.logit_scale.exp()
             
-        logits_per_image = match_avg * logit_scale
-        logits_per_text = match_avg.t() * logit_scale
+        logits_per_image = match * logit_scale
+        logits_per_text = match.t() * logit_scale
 
         return logits_per_image, logits_per_text
 
