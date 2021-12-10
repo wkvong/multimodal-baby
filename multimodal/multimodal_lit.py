f4baddee79d70d24f161e4e09992cf366f932841
Wentao Wang <wentao.wang@nyu.edu>
2021-12-10 00:17:09 -0500

    abstract common computation in both train and val into calculate_loss; add an option to optimize unused computation

--- multimodal/multimodal_lit.py
+++ multimodal/multimodal_lit.py
@@ -1,4 +1,5 @@
 import argparse
+import functools
 import copy
 import numpy as np
 import torch
@@ -31,6 +32,7 @@ class MultiModalLitModel(pl.LightningModule):
         # self.alpha = self.args.get("alpha", ALPHA)
         self.lambda_mm = self.args.get("lambda_mm", 1.)
         self.lambda_lm = self.args.get("lambda_lm", 0.)
+        self.optimize_unused = self.args.get("optimize_unused", False)
 
         self.vision_encoder = vision_encoder
         self.text_encoder = text_encoder
@@ -65,6 +67,8 @@ class MultiModalLitModel(pl.LightningModule):
                             help="multimodal loss *= lambda_mm")
         parser.add_argument("--lambda_lm", type=float, default=0.,
                             help="lm loss *= lambda_lm")
+        parser.add_argument("--optimize_unused", action="store_true",
+                            help="optimize the computation for unused loss")
         # parser.add_argument("--self_distillation", action='store_true',
         #                     help="include self-distillation loss during training")
         # parser.add_argument("--alpha", type=float, default=1.0,
@@ -78,74 +82,11 @@ class MultiModalLitModel(pl.LightningModule):
     def forward(self, x, y, y_len):
         return self.model(x, y, y_len)
 
-    def training_step(self, batch, batch_idx):
+    def calculate_loss(self, batch, stage, log):
+        # batch of image-text pairs
         x, y, y_len = batch
-        logits_per_image, logits_per_text = self(x, y, y_len)
 
-        # create ground truth labels
-        batch_size = x.size(0)
-        ground_truth = torch.tensor(
-            np.arange(batch_size), dtype=torch.long, device=self.device)
-
-        # calculate infonce loss
-        infonce_loss = (F.cross_entropy(logits_per_image, ground_truth) +
-                        F.cross_entropy(logits_per_text, ground_truth)).div(2)
-
-        # calculate accuracy (image and text separately)
-        train_image_pred = torch.argmax(logits_per_image, dim=-1)
-        train_text_pred = torch.argmax(logits_per_text, dim=-1)
-        train_image_accuracy = (
-            train_image_pred == ground_truth).sum() / batch_size
-        train_text_accuracy = (
-            train_text_pred == ground_truth).sum() / batch_size
-        train_image_entropy = get_entropy(logits_per_image, dim=-1).mean()
-        train_text_entropy = get_entropy(logits_per_text, dim=-1).mean()
-
-        # calculate self-distillation loss
-        # kl_loss = 0
-        # if self.self_distillation:
-        #     # get teacher targets and student predictions
-        #     teacher_logits_per_image, teacher_logits_per_text = self.teacher(
-        #         x, y, y_len, self_distillation=True, teacher=True)
-        #     student_logits_per_image, student_logits_per_text = self.model(
-        #         x, y, y_len, self_distillation=True, teacher=False)
-
-        #     # calculate kl div loss
-        #     kl_loss = (F.kl_div(F.log_softmax(student_logits_per_image, dim=-1), teacher_logits_per_image, reduction='batchmean') +
-        #                F.kl_div(F.log_softmax(student_logits_per_text, dim=-1), teacher_logits_per_text, reduction='batchmean')).div(2) * self.alpha
-
-        #     # update teacher model via ema
-        #     self.update_teacher()
-
-        # calculate language model ce loss
-        lm_ce_loss, perplexity, _, _, _ = self.language_model.calculate_ce_loss(
-            y, y_len)
-
-        # calculate joint train loss
-        train_loss = self.lambda_mm * infonce_loss + self.lambda_lm * lm_ce_loss
-
-        # log train loss and temperature
-        self.log("train_loss", train_loss)
-        self.log("train_infonce_loss", infonce_loss)
-        # self.log("train_kl_loss", kl_loss)
-        self.log("train_image_accuracy", train_image_accuracy)
-        self.log("train_text_accuracy", train_text_accuracy)
-        self.log("train_image_entropy", train_image_entropy)
-        self.log("train_text_entropy", train_text_entropy)
-        self.log("temperature",
-                 (-self.model.logit_neg_log_temperature).exp().item())
-        # self.log("kl_temperature",
-        #          (-self.model.kl_logit_neg_log_temperature).exp().item())
-        self.log("ce_loss", lm_ce_loss)
-        self.log("perplexity", perplexity)
-
-        return train_loss
-
-    def validation_step(self, batch, batch_idx, dataloader_idx):
-        if dataloader_idx == 0:
-            # batch of image-text pairs (same as training)
-            x, y, y_len = batch
-
+        if self.lambda_mm or not self.optimize_unused:
             logits_per_image, logits_per_text = self(x, y, y_len)
 
             # create ground truth labels
@@ -154,65 +95,109 @@ class MultiModalLitModel(pl.LightningModule):
                 np.arange(batch_size), dtype=torch.long, device=self.device)
 
             # calculate infonce loss
-            val_loss = (F.cross_entropy(logits_per_image, ground_truth) +
-                        F.cross_entropy(logits_per_text, ground_truth)).div(2)
+            infonce_loss = (
+                F.cross_entropy(logits_per_image, ground_truth) +
+                F.cross_entropy(logits_per_text, ground_truth)).div(2)
 
             # calculate accuracy (image and text separately)
-            val_image_pred = torch.argmax(logits_per_image, dim=-1)
-            val_text_pred = torch.argmax(logits_per_text, dim=-1)
-            val_image_accuracy = (
-                val_image_pred == ground_truth).sum() / batch_size
-            val_text_accuracy = (
-                val_text_pred == ground_truth).sum() / batch_size
-            val_image_entropy = get_entropy(logits_per_image, dim=-1).mean()
-            val_text_entropy = get_entropy(logits_per_text, dim=-1).mean()
+            image_pred = torch.argmax(logits_per_image, dim=-1)
+            text_pred = torch.argmax(logits_per_text, dim=-1)
+            image_accuracy = (image_pred == ground_truth).sum() / batch_size
+            text_accuracy = (text_pred == ground_truth).sum() / batch_size
+            image_entropy = get_entropy(logits_per_image, dim=-1).mean()
+            text_entropy = get_entropy(logits_per_text, dim=-1).mean()
 
+            # calculate self-distillation loss
+            # kl_loss = 0
+            # if self.self_distillation:
+            #     # get teacher targets and student predictions
+            #     teacher_logits_per_image, teacher_logits_per_text = self.teacher(
+            #         x, y, y_len, self_distillation=True, teacher=True)
+            #     student_logits_per_image, student_logits_per_text = self.model(
+            #         x, y, y_len, self_distillation=True, teacher=False)
+
+            #     # calculate kl div loss
+            #     kl_loss = (F.kl_div(F.log_softmax(student_logits_per_image, dim=-1), teacher_logits_per_image, reduction='batchmean') +
+            #                F.kl_div(F.log_softmax(student_logits_per_text, dim=-1), teacher_logits_per_text, reduction='batchmean')).div(2) * self.alpha
+
+            #     # update teacher model via ema
+            #     self.update_teacher()
+
+            # log
+            log(f"{stage}_infonce_loss", infonce_loss)
+            # log(f"{stage}_kl_loss", kl_loss)
+            log(f"{stage}_image_accuracy", image_accuracy)
+            log(f"{stage}_text_accuracy", text_accuracy)
+            log(f"{stage}_image_entropy", image_entropy)
+            log(f"{stage}_text_entropy", text_entropy)
+            log("temperature",
+                     (-self.model.logit_neg_log_temperature).exp().item())
+            # log("kl_temperature", (-self.model.kl_logit_neg_log_temperature).exp().item())
+
+        else:
+            infonce_loss = 0.
+
+        if self.lambda_lm or not self.optimize_unused:
             # calculate language model ce loss
-            lm_ce_loss, perplexity, _, _, _ = self.language_model.calculate_ce_loss(
-                y, y_len)
+            lm_ce_loss, perplexity, _, _, _ = \
+                self.language_model.calculate_ce_loss(y, y_len)
 
-            self.log("val_loss", val_loss, on_step=False, on_epoch=True)
-            self.log("val_image_accuracy", val_image_accuracy,
-                     on_step=False, on_epoch=True)
-            self.log("val_text_accuracy", val_text_accuracy,
-                     on_step=False, on_epoch=True)
-            self.log("val_image_entropy", val_image_entropy,
-                     on_step=False, on_epoch=True)
-            self.log("val_text_entropy", val_text_entropy,
-                     on_step=False, on_epoch=True)
-            self.log("val_lm_ce_loss", lm_ce_loss,
-                     on_step=False, on_epoch=True)
-            self.log("val_lm_perplexity", perplexity,
-                     on_step=False, on_epoch=True)
+            # log
+            log(f"{stage}_ce_loss", lm_ce_loss)
+            log(f"{stage}_perplexity", perplexity)
 
-            return val_loss
+        else:
+            lm_ce_loss = 0.
+
+        # calculate joint loss
+        loss = self.lambda_mm * infonce_loss + self.lambda_lm * lm_ce_loss
+
+        # log
+        log(f"{stage}_loss", loss)
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self.calculate_loss(batch, 'train', self.log)
+
+    def validation_step(self, batch, batch_idx, dataloader_idx):
+        stage = 'val'
+        log = functools.partial(self.log, on_step=False, on_epoch=True)
+
+        if dataloader_idx == 0:
+            return self.calculate_loss(batch, stage, log)
+
         elif dataloader_idx == 1:
+            # TODO: check whether adding special tokens will make a difference
+
             # batch of evaluation trials (only one trial at a time)
             x, y, y_len = batch
 
-            # resize x so images from the same trial are in the batch dim
-            # [B, N, C, H, W] -> [B*N, C, H, W]  (with B = 1)
-            x = x.view(-1, *x.shape[-3:])
+            if self.lambda_mm or not self.optimize_unused:
+                # resize x so images from the same trial are in the batch dim
+                # [B, N, C, H, W] -> [B*N, C, H, W]  (with B = 1)
+                x = x.view(-1, *x.shape[-3:])
 
-            # calculate accuracy
-            logits_per_image, logits_per_text = self.model(x, y, y_len)
-            logits = logits_per_text[0]  # get logits per trial
-            pred = torch.argmax(logits).item()
-            label = 0  # correct answer is always the first item
-            val_accuracy = int(pred == label)
-            val_entropy = get_entropy(logits)
+                # calculate accuracy
+                logits_per_image, logits_per_text = self.model(x, y, y_len)
+                logits = logits_per_text[0]  # get logits per trial
+                pred = torch.argmax(logits).item()
+                label = 0  # correct answer is always the first item
+                accuracy = int(pred == label)
+                entropy = get_entropy(logits)
 
-            # log evaluation accuracy and entropy
-            self.log("val_accuracy", val_accuracy,
-                     on_step=False, on_epoch=True)
-            self.log("val_entropy", val_entropy, on_step=False, on_epoch=True)
+                # log evaluation accuracy and entropy
+                log(f"{stage}_accuracy", accuracy)
+                log(f"{stage}_entropy", entropy)
 
-            # log category-level evaluation accuracies as a separate metric
-            category_label = self.text_encoder.idx2word[y.item()]
-            self.log(f"val_accuracy_{category_label}",
-                     val_accuracy, on_step=False, on_epoch=True)
+                # log category-level evaluation accuracies as a separate metric
+                category_label = self.text_encoder.idx2word[y.item()]
+                log(f"{stage}_accuracy_{category_label}", accuracy)
 
-            return val_accuracy
+            else:
+                accuracy = 0.
+
+            return accuracy
 
     # def update_teacher(self):
     #     for teacher, student in zip(self.teacher.parameters(), self.model.parameters()):
