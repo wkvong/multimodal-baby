28d155581ea94332cbff6b94410d7a211b1495f7
Wentao Wang <wentao.wang@nyu.edu>
2022-11-13 18:03:44 -0500

    further abstract frame subplots

--- analysis_tools/frame_visualization.py
+++ analysis_tools/frame_visualization.py
@@ -28,13 +28,14 @@ def untokenize(utterance):
     return re.sub(r" (?=([\.\?\!,']|((n('|)t|na)\b)))", r"", utterance)
 
 
-def get_wrap_width(fontsize, frame_width, c=96.):
+def get_wrap_width(fontsize, frame_width, c=100.):
     return int(c * frame_width / fontsize)
 
 
 def add_caption(
     ax,
     caption,
+    x=0.5, y=-0.05,
     fontsize=5.,
     wrap_width=None,
     frame_width=None,
@@ -43,8 +44,8 @@ def add_caption(
     if wrap_width is None:
         wrap_width = get_wrap_width(fontsize, frame_width)
     return ax.text(
-        0.5, -0.05,
-        "\n".join(wrap(caption[:100], wrap_width)),
+        x, y,
+        "\n".join(wrap(caption, wrap_width)),
         ha='center', va='top',
         transform=ax.transAxes,
         fontsize=fontsize,
